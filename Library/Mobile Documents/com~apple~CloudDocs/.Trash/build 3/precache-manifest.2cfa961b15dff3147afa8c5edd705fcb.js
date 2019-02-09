3507ca82c3a47d4bc7f808ea55d5c90a595ab0b6
svetagimpelson <svetagimpelson@se.shenkar.ac.il>
2019-02-09 19:56:18 +0200

    final

---
+++ Library/Mobile Documents/com~apple~CloudDocs/.Trash/build 3/precache-manifest.2cfa961b15dff3147afa8c5edd705fcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4576d7e7cffe20ba3a52",
+    "url": "/static/js/main.4576d7e7.chunk.js"
+  },
+  {
+    "revision": "9c4c39b366c116fb366d",
+    "url": "/static/js/1.9c4c39b3.chunk.js"
+  },
+  {
+    "revision": "4576d7e7cffe20ba3a52",
+    "url": "/static/css/main.655cd38e.chunk.css"
+  },
+  {
+    "revision": "1e4de9f8fed609c3af711c0f52fe1ae6",
+    "url": "/index.html"
+  }
+];
